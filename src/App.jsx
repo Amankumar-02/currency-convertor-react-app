56ac750ccb844cc7521a3fdae7598d8883f9bb7a
Aman <118505622+Phoenix2868@users.noreply.github.com>
2023-09-25 13:53:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import {InputBox} from './components'
 import useCurrencyInfo from './hooks/useCurrencyInfo'
 
 function App() {
-  const [amount, setAmount] = useState(0);
+  const [amount, setAmount] = useState();
   // const [currency, setCurrency] = useState();
   const [from, setFrom] = useState("usd");
   const [to, setTo] = useState("inr");
-  const [convertedAmount, setConvertedAmount] = useState(0);
+  const [convertedAmount, setConvertedAmount] = useState();
 
   const currencyInfo = useCurrencyInfo(from);
   const options = Object.keys(currencyInfo);
