cd0e9fe3cf383433b06a75d0870fb4252ed9d57f
Aman <ak193566@gmail.com>
2024-01-10 18:16:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
     <div
         className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
         style={{
-            backgroundImage: `url('https://cdn.pixabay.com/photo/2023/09/09/10/25/mountains-8242910_1280.png')`,
+            backgroundImage: `url('https://images.unsplash.com/photo-1704071085149-2dd6f2e0a37a?q=80&w=1503&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`,
         }}
     >
         <div className="w-full">
